b72158bc906981ceadadb086fd7fd4eafc6ab12b
Klara923 <117095791+Klara923@users.noreply.github.com>
2026-06-08 19:12:28 +0200

    made the screen responsive

--- lunalander.js
+++ lunalander.js
@@ -1,27 +1,48 @@
-// setup of buttons
+const GW = 600;
+const GH = 500;
+let gameScale = 1;
+let offsetX = 0;
+let offsetY = 0;
+
+function updateLayout() {
+  gameScale = Math.min(windowWidth / GW, windowHeight / GH);
+  offsetX = (windowWidth - GW * gameScale) / 2;
+  offsetY = (windowHeight - GH * gameScale) / 2;
+
+  if (button) {
+    button.position(offsetX + 270 * gameScale, offsetY + 450 * gameScale);
+    button.size(100 * gameScale, 30 * gameScale);
+  }
+  if (button1) {
+    button1.position(offsetX + 265 * gameScale, offsetY + 420 * gameScale);
+    button1.size(100 * gameScale, 30 * gameScale);
+  }
+}
+
 function setup() {
-  createCanvas(600, 500);
+  createCanvas(windowWidth, windowHeight);
   button = createButton("Play again!");
   button.mousePressed(playAgain);
-  button.position(270, 450);
   button.style("color:white");
   button.style("background-color: rgb(155, 0, 0)");
   button.style("border-radius", "20px");
   button.style("border", "none");
-  button.size(100, 30);
   button.hide();
   button1 = createButton("Start a game!");
   button1.hide();
   button1.mousePressed(startGame);
-  button1.position(265, 420);
   button1.style("color:white");
   button1.style("background-color: rgb(155, 0, 0)");
   button1.style("border-radius", "20px");
   button1.style("border", "none");
-  button1.size(100, 30);
+  updateLayout();
 }
 
-// variables
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  updateLayout();
+}
+
 let catY = 100;
 let catX = 300;
 let velocity = 0.1;
@@ -37,21 +58,18 @@ let x3 = 100;
 let y3 = 0;
 let s3 = 1;
 let pic1;
-// moon image preload
+
 function preload() {
   pic1 = loadImage("moon.png");
 }
 
-// cat
 function emoji(catX, catY, s) {
   translate(0, 0);
-  // Cat head
   fill(0, 0, 0);
   strokeWeight(2);
   stroke(255, 255, 255);
   ellipse(catX, catY, 200 * s);
 
-  // Cat ears
   beginShape();
   vertex(catX - 100 * s, catY - 0 * s);
   vertex(catX - 100 * s, catY - 100 * s);
@@ -64,20 +82,16 @@ function emoji(catX, catY, s) {
   vertex(catX + 100 * s, catY + 1 * s);
   endShape();
 
-  // Cat eyes
   fill(155, 0, 0);
   ellipse(catX - 50 * s, catY + 30 * s, 20 * s);
   ellipse(catX + 40 * s, catY + 30 * s, 20 * s);
 
-  // Cat nose
   noStroke();
   beginShape();
   vertex(catX - 5 * s, catY + 50 * s);
   vertex(catX + 1 * s, catY + 40 * s);
   vertex(catX - 10 * s, catY + 40 * s);
   endShape();
-
-  // moustaches
 
   fill(0, 0, 0);
   stroke(255, 255, 255);
@@ -89,7 +103,6 @@ function emoji(catX, catY, s) {
   line(catX + 90 * s, catY + 40 * s, catX + 140 * s, catY + 40 * s);
   line(catX + 85 * s, catY + 50 * s, catX + 135 * s, catY + 80 * s);
 
-  // Cat horns
   fill(255, 255, 255);
   noStroke();
   beginShape();
@@ -106,24 +119,19 @@ function emoji(catX, catY, s) {
   endShape();
 }
 
-// starts - array and loop
-
 let stars = [];
 
 for (let i = 0; i < 100; i++) {
   const star = {
-    x: Math.floor(Math.random() * 600),
-    y: Math.floor(Math.random() * 600),
+    x: Math.floor(Math.random() * GW),
+    y: Math.floor(Math.random() * GH),
     alpha: Math.random(),
   };
   stars.push(star);
 }
 
-// screen1 = start screen
-// The following 18 lines of code was adapted from Garrit's video
 function screen1() {
   background(0, 0, 0);
-  // stars
   for (let star of stars) {
     fill(255, 255, 255, Math.abs(Math.sin(star.alpha) * 255));
 
@@ -132,14 +140,13 @@ function screen1() {
     star.x = star.x + 0.1;
     star.y = star.y + 0.1;
 
-    if (star.x > width) {
+    if (star.x > GW) {
       star.x = 0;
     }
-    if (star.y > height) {
+    if (star.y > GH) {
       star.y = 0;
     }
   }
-  // text
   fill(255, 255, 255);
   textSize(40);
   text("Cat Lander Game", 150, 200, 500, 500);
@@ -154,14 +161,11 @@ function screen1() {
   button1.show();
 }
 
-// alpha for flashing dog eyes
 let alpha = 0;
 
-// screen2 = game screen
 function screen2() {
   background(0, 0, 0);
   background(0, 0, 0);
-  // The following 15 lines of code was adapted from Garrit's video
   for (let star of stars) {
     fill(255, 255, 255, Math.abs(Math.sin(star.alpha) * 255));
 
@@ -170,26 +174,23 @@ function screen2() {
     star.x = star.x + 0.1;
     star.y = star.y + 0.1;
 
-    if (star.x > width) {
+    if (star.x > GW) {
       star.x = 0;
     }
-    if (star.y > height) {
+    if (star.y > GH) {
       star.y = 0;
     }
   }
 
-  // dog house
   push();
   stroke(255, 255, 255);
   strokeWeight(5);
   fill(0, 0, 0);
   rect(100, 430, 80, 66);
-  // black line
   strokeWeight(6);
   stroke(0, 0, 0);
   fill(0, 0, 0);
   line(99, 430, 182, 430);
-  // roof
   stroke(255, 255, 255);
   strokeWeight(5);
   fill(0, 0, 0);
@@ -197,14 +198,12 @@ function screen2() {
   line(185, 438, 139, 400);
   push();
 
-  // door
   translate(139, 445);
   rotate(PI);
   noFill();
   strokeWeight(5);
   stroke(255, 255, 255);
   arc(0, -50, 45, 90, 0, PI);
-  // dog eyes
   pop();
   noStroke();
   fill(155, 0, 0, Math.abs(Math.sin(alpha) * 255));
@@ -216,7 +215,6 @@ function screen2() {
   alpha = alpha + 0.3;
   pop();
 
-  // cat house
   push();
   translate(420, 445);
   rotate(PI);
@@ -251,14 +249,11 @@ function screen2() {
   endShape();
   pop();
 
-  // button is not showing
   button1.hide();
 }
 
-// screen3 = first ending, cat is dead
 function screen3() {
   background(0, 0, 0);
-  // The following 15 lines of code was adapted from Garrit's video
   for (let star of stars) {
     fill(255, 255, 255, Math.abs(Math.sin(star.alpha) * 255));
 
@@ -267,32 +262,26 @@ function screen3() {
     star.x = star.x + 0.1;
     star.y = star.y + 0.1;
 
-    if (star.x > width) {
+    if (star.x > GW) {
       star.x = 0;
     }
-    if (star.y > height) {
+    if (star.y > GH) {
       star.y = 0;
     }
   }
-  // text
   fill(255, 255, 255);
   textSize(35);
   text("Oops! You killed a kitten.", 120, 380, 500, 500);
-  // button play again
   button.show();
 }
 
-// dead cat
-
 function emojix(x1, y1, s1) {
   translate(0, 0);
-  // Cat head
   fill(0, 0, 0);
   strokeWeight(2);
   stroke(255, 255, 255);
   ellipse(x1, y1, 200 * s1);
 
-  // Cat ears
   beginShape();
   vertex(x1 - 100 * s1, y1 - 0 * s1);
   vertex(x1 - 100 * s1, y1 - 100 * s1);
@@ -305,7 +294,6 @@ function emojix(x1, y1, s1) {
   vertex(x1 + 100 * s1, y1 + 1 * s1);
   endShape();
 
-  // Cat eyes
   push();
   strokeWeight(7);
   fill(255, 255, 255);
@@ -315,7 +303,6 @@ function emojix(x1, y1, s1) {
   line(x1 + 30 * s1, y1 + 40 * s1, x1 + 60 * s1, y1 + 10 * s1);
   pop();
 
-  // Cat nose
   noStroke();
   fill(155, 0, 0);
   beginShape();
@@ -323,8 +310,6 @@ function emojix(x1, y1, s1) {
   vertex(x1 + 1 * s1, y1 + 40 * s1);
   vertex(x1 - 10 * s1, y1 + 40 * s1);
   endShape();
-
-  // moustaches
 
   fill(0, 0, 0);
   stroke(255, 255, 255);
@@ -336,7 +321,6 @@ function emojix(x1, y1, s1) {
   line(x1 + 90 * s1, y1 + 40 * s1, x1 + 140 * s1, y1 + 40 * s1);
   line(x1 + 85 * s1, y1 + 50 * s1, x1 + 135 * s1, y1 + 80 * s1);
 
-  // Cat horns
   fill(255, 255, 255);
   noStroke();
   beginShape();
@@ -353,11 +337,8 @@ function emojix(x1, y1, s1) {
   endShape();
 }
 
-// screen4 = second ending, cat sleeps
 function screen4() {
   background(0, 0, 0);
-  // The following 16 lines of code was adapted from Garrit's video
-  // stars
   for (let star of stars) {
     fill(255, 255, 255, Math.abs(Math.sin(star.alpha) * 255));
 
@@ -366,29 +347,25 @@ function screen4() {
     star.x = star.x + 0.1;
     star.y = star.y + 0.1;
 
-    if (star.x > width) {
+    if (star.x > GW) {
       star.x = 0;
     }
-    if (star.y > height) {
+    if (star.y > GH) {
       star.y = 0;
     }
   }
-  // text
   fill(255, 255, 255);
   textSize(35);
   text("Yay! You put the kitten to bed!", 60, 350, 500, 500);
-  // button play again
   button.show();
 }
 
 function emojizzz(x2, y2, s2) {
-  // Cat head
   fill(0, 0, 0);
   strokeWeight(2);
   stroke(255, 255, 255);
   ellipse(x2, y2, 200 * s2);
 
-  // Cat ears
   beginShape();
   vertex(x2 - 100 * s2, y2 - 0 * s2);
   vertex(x2 - 100 * s2, y2 - 100 * s2);
@@ -401,7 +378,6 @@ function emojizzz(x2, y2, s2) {
   vertex(x2 + 100 * s2, y2 + 1 * s2);
   endShape();
 
-  // Cat nose
   noStroke();
   fill(155, 0, 0);
   beginShape();
@@ -409,8 +385,6 @@ function emojizzz(x2, y2, s2) {
   vertex(x2 + 1 * s2, y2 + 40 * s2);
   vertex(x2 - 10 * s2, y2 + 40 * s2);
   endShape();
-
-  // moustaches
 
   fill(0, 0, 0);
   stroke(255, 255, 255);
@@ -422,7 +396,6 @@ function emojizzz(x2, y2, s2) {
   line(x2 + 90 * s2, y2 + 40 * s2, x2 + 140 * s2, y2 + 40 * s2);
   line(x2 + 85 * s2, y2 + 50 * s2, x2 + 135 * s2, y2 + 80 * s2);
 
-  // Cat horns
   fill(255, 255, 255);
   noStroke();
   beginShape();
@@ -430,7 +403,6 @@ function emojizzz(x2, y2, s2) {
   vertex(x2 - 30 * s2, y2 - 80 * s2);
   vertex(x2 - 50 * s2, y2 - 80 * s2);
   endShape();
-  // Cat eyes
   fill(255, 255, 255);
   beginShape();
   vertex(x2 + 30 * s2, y2 - 80 * s2);
@@ -446,7 +418,7 @@ function emojizzz(x2, y2, s2) {
   arc(x2 + 50 * s2, y2 + 30 * s2, 12 * s2, 30 * s2, 0 * s2, PI);
   pop();
 }
-// "zzz"
+
 function z(x3, y3) {
   fill(255, 255, 255);
   textSize(30);
@@ -460,6 +432,10 @@ function z(x3, y3) {
 state = "start";
 
 function draw() {
+  background(0, 0, 0);
+  push();
+  translate(offsetX, offsetY);
+  scale(gameScale);
   if (state === "start") {
     screen1();
     emoji(300, 100, 0.3);
@@ -483,14 +459,12 @@ function draw() {
       }
     }
   }
-  // controlling cat with the keys
   if (keyIsDown(37)) {
     catX = catX - 1;
   }
   if (keyIsDown(39)) {
     catX = catX + 1;
   }
-  // if cat is in certain position one of endings(screens)is displaying
   if (catX > 30 && catX < 150 && catY > 350) {
     state = "xx";
   }
@@ -508,12 +482,13 @@ function draw() {
     emojizzz(x2, y2, s2);
     z(x3, y3);
   }
+  pop();
 }
-// function for button of the first screen, if you press it, state = "game" so you start playing
+
 function startGame() {
   state = "game";
 }
-// function for button1 on the screen3 and screen4, when you click it state = "start" so you start from the beggining (screen1)
+
 function playAgain() {
   state = "start";
   catY = 100;
